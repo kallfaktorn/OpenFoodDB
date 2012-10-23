10e7fc12fdf22bd828e61674a06adf84ec47ae6d
ls <mattiaspgames@gmail.com>
2012-10-23 13:23:14 +0200

    Sign in and greetings working and correctly placed in correct views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,2 @@
 module ApplicationHelper
-
-  def user_signed_in
-    if current_user
-      print "wohoooooooooooooooooooooooooooooooo"
-      print "wohoooooooooooooooooooooooooooooooo"
-      print "wohooooooooooooooooooooooooooooooow"
-      print current_user.name
-      return true
-    else
-      print "blooooooooooooooooooooooooooooooooo"
-      print "blhoooooooooooooooooooooooooooooooo"
-      print "bloooooooooooooooooooooooooooooooow"
-      print current_user.name
-      return false
-    end
-  end
 end
