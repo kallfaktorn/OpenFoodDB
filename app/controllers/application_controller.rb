28adff6c9c0f30620fbda68a7850e6cc52aa0114
ls <mattiaspgames@gmail.com>
2012-10-24 19:45:56 +0200

    Added locale to each URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
    protect_from_forgery
   include SessionsHelper
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
+  end
 end
